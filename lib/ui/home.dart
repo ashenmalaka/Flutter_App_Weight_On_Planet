7f58cd4f32a9ca3191bf4fd573199011d05d5b42
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2019-12-22 13:09:58 +0530

    Added another displaying text

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -131,7 +131,8 @@ class HomeState extends State<Home> {
 
                   //Result Text
                   new Text(
-                    "$_formattedText lbs",
+                    _weightController.text.isEmpty ? "Please Enter Your Weight" : _formattedText + "lbs",
+                    //"$_formattedText lbs",
                     style: new TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.w500,
